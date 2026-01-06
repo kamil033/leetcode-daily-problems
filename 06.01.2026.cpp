f39d94206db745ba9123d7d7d698df21eff4b680
MOHD KAMIL <mkamilkamil856@gmail.com>
2026-01-06 20:28:02 +0530

    1161. Maximum Level Sum of a Binary Tree

---
+++ 06.01.2026.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        queue<TreeNode*> q{{root}};
+        int mx = INT_MIN;
+        int ans = 0;
+        int i = 0;
+        while (!q.empty()) {
+            ++i;
+            int s = 0;
+            for (int n = q.size(); n; --n) {
+                root = q.front();
+                q.pop();
+                s += root->val;
+                if (root->left) {
+                    q.push(root->left);
+                }
+                if (root->right) {
+                    q.push(root->right);
+                }
+            }
+            if (mx < s) {
+                mx = s;
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
