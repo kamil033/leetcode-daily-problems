a529e62584e75ca468cb59d1a65398d559091220
MOHD KAMIL <mkamilkamil856@gmail.com>
2026-02-09 10:43:24 +0530

    1382. Balance a Binary Search Tree

---
+++ 09.02.2026.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    TreeNode* balanceBST(TreeNode* root) {
+        dfs(root);
+        return build(0, nums.size() - 1);
+    }
+
+private:
+    vector<int> nums;
+
+    void dfs(TreeNode* root) {
+        if (!root) {
+            return;
+        }
+        dfs(root->left);
+        nums.push_back(root->val);
+        dfs(root->right);
+    }
+
+    TreeNode* build(int i, int j) {
+        if (i > j) {
+            return nullptr;
+        }
+        int mid = (i + j) >> 1;
+        TreeNode* left = build(i, mid - 1);
+        TreeNode* right = build(mid + 1, j);
+        return new TreeNode(nums[mid], left, right);
+    }
+};
